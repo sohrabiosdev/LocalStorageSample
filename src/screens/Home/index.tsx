a7b8c3eb4d47f74cde61ff7bcca9b2973aaa5fc9
Sohrab Hussain <sohrab.hussain@eres.ae>
2019-08-28 14:36:42 +0400

    Updated Home UI.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -21,7 +21,7 @@ export default class Home extends Component<Prop, State> {
   }
 
   private saveDataSQLite() {
-    this.props.navigation.navigate('AsyncStorageView');
+    this.props.navigation.navigate('SQLiteView');
   }
 
   private saveDataRealm() {
@@ -41,33 +41,63 @@ export default class Home extends Component<Prop, State> {
       <View style={styles.container}>
         <Text style={{textAlign: 'center'}}>{'Local Storage Options'}</Text>
         <TouchableOpacity onPress={() => this.saveDataAsyncStorage()}>
-          <View style={{backgroundColor: 'green', padding: 10, marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: 'green',
+              padding: 10,
+              marginTop: 20,
+              borderRadius: 10,
+            }}>
             <Text style={{color: 'white', textAlign: 'center'}}>
               {'Async Storage'}
             </Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.saveDataSQLite()}>
-          <View style={{backgroundColor: 'green', padding: 10, marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: 'green',
+              padding: 10,
+              marginTop: 20,
+              borderRadius: 10,
+            }}>
             <Text style={{color: 'white', textAlign: 'center'}}>
-              {'SQLite 2'}
+              {'SQLite'}
             </Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.saveDataRealm()}>
-          <View style={{backgroundColor: 'green', padding: 10, marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: 'green',
+              padding: 10,
+              marginTop: 20,
+              borderRadius: 10,
+            }}>
             <Text style={{color: 'white', textAlign: 'center'}}>{'Realm'}</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.saveDataMongoDB()}>
-          <View style={{backgroundColor: 'green', padding: 10, marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: 'green',
+              padding: 10,
+              marginTop: 20,
+              borderRadius: 10,
+            }}>
             <Text style={{color: 'white', textAlign: 'center'}}>
               {'Mongo DB'}
             </Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.saveDataFirebase()}>
-          <View style={{backgroundColor: 'green', padding: 10, marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: 'green',
+              padding: 10,
+              marginTop: 20,
+              borderRadius: 10,
+            }}>
             <Text style={{color: 'white', textAlign: 'center'}}>
               {'Firebase'}
             </Text>
